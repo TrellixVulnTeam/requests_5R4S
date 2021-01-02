4263b22248c9b8c907a5f4021bc6540df6e09919
newhackerman <newhackerman@163.com>
2021-01-02 22:36:23 +0800

    ds

--- Request-demo1.py
+++ Request-demo1.py
@@ -1,9 +1,12 @@
 import requests
-if __name__ == '__main__':
-    url="https://www.sogou.com/"
-    response=requests.get(url=url)
-    responsetext=response.text
-    print(responsetext)
-    with open('sogou.html','w',encoding='utf-8') as fw:
-        fw.write(responsetext)
 
+def request_demo1(url):
+
+    response = requests.get(url=url)
+    responsetext = response.text
+    print(responsetext)
+    with open('sogou.html', 'w', encoding='utf-8') as fw:
+        fw.write(responsetext)
+if __name__ == '__main__':
+    url = "https://www.sogou.com/"
+    request_demo1(url)
